01bf0da8662e01f0a551e2373c5192a22cd11468
dbvhPhuoc <147008949+dbvhPhuoc@users.noreply.github.com>
2024-08-09 14:47:55 +0700

    Update AccountServiceConstructorImpl.java

--- day_2/src/main/java/dependencyinjection/AccountServiceConstructorImpl.java
+++ day_2/src/main/java/dependencyinjection/AccountServiceConstructorImpl.java
@@ -1,4 +1,28 @@
 package dependencyinjection;
 
-public class AccountServiceConstructorImpl {
+import pojobeans.Account;
+import pojobeans.AccountRepository;
+import pojobeans.AccountService;
+
+public class AccountServiceConstructorImpl implements AccountService{
+    private AccountRepository accountRepository;
+
+    public AccountServiceConstructorImpl(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    @Override
+    public void transferMoney(long fromAccountId, long toAccountId, double amount) {
+
+    }
+
+    @Override
+    public void depositMoney(long accountId, double amount) throws Exception {
+
+    }
+
+    @Override
+    public Account getAccount(long accountId) {
+        return null;
+    }
 }
