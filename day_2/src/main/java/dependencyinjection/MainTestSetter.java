3ff9e55b88af3ad2309371685ee738c9e67986c1
dbvhPhuoc <147008949+dbvhPhuoc@users.noreply.github.com>
2024-08-09 14:44:40 +0700

    Update MainTestSetter.java

--- day_2/src/main/java/dependencyinjection/MainTestSetter.java
+++ day_2/src/main/java/dependencyinjection/MainTestSetter.java
@@ -2,10 +2,21 @@ package dependencyinjection;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pojobeans.AccountService;
 
 public class MainTestSetter {
-    public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
-    }
+        public static void main (String[] args) {
+            ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("xmlbasedconfiguration/beans.xml");
+            AccountService accountService = applicationContext.getBean("accountService", AccountService.class);
+            System.out.println("Before money transfer");
+            System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+            System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+
+            accountService.transferMoney(1, 2, 5.0);
+            System.out.println("===================");
+            System.out.println("After money transfer");
+            System.out.println("Account 1 balance :" + accountService.getAccount(1).getBalance());
+            System.out.println("Account 2 balance :" + accountService.getAccount(2).getBalance());
+        }
 }
